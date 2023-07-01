80aebadaae91e64fb40e1957d30f9b9eec8a93df
TiFcode <35370681+TiFcode@users.noreply.github.com>
2023-07-01 06:58:01 +0300

    BERT - get similarity between sentences

--- bert.py
+++ bert.py
@@ -1,4 +1,6 @@
+from scipy.spatial.distance import cosine
 from transformers import BertModel, BertTokenizer
+import torch
 
 def get_bert_embeddings(sentence):
 
@@ -7,12 +9,12 @@ def get_bert_embeddings(sentence):
     model = BertModel.from_pretrained('bert-base-uncased')
 
     inputs = tokenizer(sentence, return_tensors='pt')
+    # print("inputs:")
+    # print(inputs)
 
-    print("inputs:")
-    print(inputs)
     outputs = model(**inputs)
-    print("outputs:")
-    print(outputs)
+    # print("outputs:")
+    # print(outputs)
 
     # The BERT model returns 2 values:
     # 1. last_hidden_state (sequence of hidden-states at the output of the last layer of the model)
@@ -23,12 +25,40 @@ def get_bert_embeddings(sentence):
 
     return last_hidden_state, pooler_output
 
+def get_sentence_similarity(sentence1, sentence2):
+
+    print("get_sentence_similarity() received these sentences as input:")
+    print("[" + sentence1 + "]")
+    print("[" + sentence2 + "]")
+
+    _, pooler_output1 = get_bert_embeddings(sentence1)
+    _, pooler_output2 = get_bert_embeddings(sentence2)
+
+    # We detach the tensors from the computational graph and convert them to numpy arrays for use with scipy
+    pooler_output1 = pooler_output1.detach().numpy().squeeze()
+    pooler_output2 = pooler_output2.detach().numpy().squeeze()
+
+    similarity = 1 - cosine(pooler_output1, pooler_output2)
+
+    return similarity
+
 if __name__ == "__main__":
     sentence = "This is a sample sentence for BERT."
     last_hidden_state, pooler_output = get_bert_embeddings(sentence)
-    print("last_hidden_state:")
-    print(last_hidden_state)
-    print("pooler_output:")
-    print(pooler_output)
+    # print("last_hidden_state:")
+    # print(last_hidden_state)
+    # print("pooler_output:")
+    # print(pooler_output)
     print("Last hidden state shape:", last_hidden_state.shape)
     print("Pooler output shape:", pooler_output.shape)
+
+    sentence1 = "This is a sample sentence for BERT."
+    sentence2 = "BERT is used in this sample sentence."
+    sentence3 = "I love to play football."
+    print("\n\n")
+    print("Sentence1 == " + sentence1)
+    print("Sentence2 == " + sentence2)
+    print("Sentence3 == " + sentence3)
+
+    print("Similarity between sentence 1 and 2:", get_sentence_similarity(sentence1, sentence2))
+    print("Similarity between sentence 1 and 3:", get_sentence_similarity(sentence1, sentence3))
